441bd7ba3971c0c70fe2f43e99e9bd68ea132bb5
Henry <t.henry.chesnutt@gmail.com>
2019-03-28 22:13:57 -0700

    refactor to single setState

--- src/App.js
+++ src/App.js
@@ -5,103 +5,130 @@ import Filters from './components/Filters'
 import ConditionalRender from './components/base-ui/ConditionalRender'
 import { AppWrapper, FilterContainer, ContentWrapper } from './styledComponents'
 import { Button } from 'semantic-ui-react'
+
 import { scooters } from './data/scooters'
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      displayFilters: true,
-      filtersAdded: false,
-      Scooter: true,
-      Skateboard: true,
-      Alternative: true,
-      minPrice: 0,
-      maxPrice: 1500,
-      minWeight: 0,
-      maxWeight: 1500,
-      minSpeed: 0,
-      maxSpeed: 1500,
-      minRange: 0,
-      maxRange: 1500,
-      minPower: 0,
-      maxPower: 1500,
-      filteredScooters: scooters
+      filters: {
+        displayFilters: true,
+        filtersAdded: false,
+        Scooter: true,
+        Skateboard: true,
+        Alternative: true,
+        minPrice: 0,
+        maxPrice: 1500,
+        minWeight: 0,
+        maxWeight: 1500,
+        minSpeed: 0,
+        maxSpeed: 1500,
+        minRange: 0,
+        maxRange: 1500,
+        minPower: 0,
+        maxPower: 1500,
+      },
+      filteredScooters: scooters,
     }
+    this.filterScooters = this.filterScooters.bind(this);
     this.openCloseFilters = this.openCloseFilters.bind(this);
     this.handlePriceFilter = this.handlePriceFilter.bind(this);
     this.handleSpeedFilter = this.handleSpeedFilter.bind(this);
-    this.handleRangeFilter = this.handleRangeFilter.bind(this);   this.handleWeightFilter = this.handleWeightFilter.bind(this);
+    this.handleRangeFilter = this.handleRangeFilter.bind(this);   
+    this.handleWeightFilter = this.handleWeightFilter.bind(this);
     this.handlePowerFilter = this.handlePowerFilter.bind(this);
     this.handleTypeFilter = this.handleTypeFilter.bind(this);
   }
+
   async openCloseFilters() {
     const { displayFilters } = this.state;
     this.setState({displayFilters: !displayFilters})
   }
-  handleFilter() {
-    const { minPrice, maxPrice, minSpeed, maxSpeed, minRange, maxRange, minWeight, maxWeight, minPower, maxPower} = this.state;
-    const filteredScooters = scooters.filter(scooter => 
-    (scooter.price > minPrice && scooter.price < maxPrice &&
-    scooter.speed > minSpeed && scooter.speed < maxSpeed &&
-    scooter.range > minRange && scooter.range < maxRange &&
-    scooter.weight > minWeight && scooter.weight < maxWeight
-    ))
-    this.setState({filteredScooters})
+
+  filterScooters(filters) {
+    const filteredScooters = scooters.filter(scooter => {
+      const { minPrice, maxPrice, minSpeed, maxSpeed, minRange, maxRange, minWeight, maxWeight, minPower, maxPower} = filters;
+
+      const inPriceRange = scooter.price > minPrice && scooter.price < maxPrice;
+      const inSpeedRange = scooter.speed > minSpeed && scooter.speed < maxSpeed;
+      const inRangeRange = scooter.range > minRange && scooter.range < maxRange;
+      const inWeightRange = scooter.weight > minWeight && scooter.weight < maxWeight;
+      return inPriceRange && inSpeedRange && inRangeRange && inWeightRange;
+    });
+    return filteredScooters;
   }
-  async handlePriceFilter(minPrice, maxPrice) {
-    await this.setState({minPrice, maxPrice})
-    this.handleFilter();
+  
+  handleFilter(filterChanges) {
+    const newFilters = {
+      ...this.state.filters,
+      ...filterChanges,
+    };
+    console.log('handleFilters', newFilters)
+    const filteredScooters = this.filterScooters(newFilters)
+    this.setState({ filteredScooters, 
+      newFilters,
+    });
   }
-  async handleSpeedFilter(minSpeed, maxSpeed) {
-    await this.setState({minSpeed, maxSpeed})
-    this.handleFilter();
+
+  handlePriceFilter(minPrice, maxPrice) {
+    const filterChanges = {
+      minPrice,
+      maxPrice,
+    }
+    this.handleFilter(filterChanges);
   }
-  async handleRangeFilter(minRange, maxRange) {
-    await this.setState({minRange, maxRange})
-    this.handleFilter();
+
+  handleSpeedFilter(minSpeed, maxSpeed) {
+    this.handleFilter({ minSpeed, maxSpeed });
   }
-  async handleWeightFilter(minWeight, maxWeight) {
-    await this.setState({minWeight, maxWeight})
-    this.handleFilter();
+
+  handleRangeFilter(minRange, maxRange) {
+    this.handleFilter({ minRange, maxRange });
   }
-  async handlePowerFilter(minPower, maxPower) {
-    await this.setState({minPower, maxPower})
-    this.handleFilter();
+
+  handleWeightFilter(minWeight, maxWeight) {
+    this.handleFilter({ minWeight, maxWeight });
   }
-  async handleTypeFilter(option) {
-    await this.setState({[option]: !this.state[option]})
-    this.handleFilter()
+
+  handlePowerFilter(minPower, maxPower) {
+    this.handleFilter({ minPower, maxPower });
   }
+
+  handleTypeFilter(option) {
+    this.handleFilter({ [option]: !this.state[option] })
+  }
+
   render() {
     const { displayFilters, filteredScooters, Scooter, Skateboard, Alternative } = this.state;
     return (
       <AppWrapper>
         <Header/>
         <ContentWrapper>
-        <FilterContainer>
-        <Button
-          content='Add Filters'
-          toggle
-          active={displayFilters}
-          onClick={this.openCloseFilters}
-          style={{marginTop: 20, color: 'black'}}
-          size='big'
-        />
-        <ConditionalRender 
-          shouldRender={displayFilters}
-          component={Filters}
-          openCloseFilters={this.openCloseFilters}
-          handlePriceFilter={this.handlePriceFilter}
-          handleSpeedFilter={this.handleSpeedFilter}
-          handleRangeFilter={this.handleRangeFilter}
-          handlePowerFilter={this.handlePowerFilter}
-          handleWeightFilter={this.handleWeightFilter}
-          handleTypeFilter={this.handleTypeFilter}
-        />
-        </FilterContainer>
-        <Cards 
-          scooters={filteredScooters}
-        />
+          <FilterContainer>
+            <Button
+              content='Add Filters'
+              toggle
+              active={displayFilters}
+              onClick={this.openCloseFilters}
+              style={{marginTop: 20, color: 'black'}}
+              size='big'
+            />
+            <ConditionalRender 
+              shouldRender={displayFilters}
+              component={Filters}
+              openCloseFilters={(this.openCloseFilters)}
+              handlePriceFilter={this.handlePriceFilter}
+              handleSpeedFilter={this.handleSpeedFilter}
+              handleRangeFilter={this.handleRangeFilter}
+              handlePowerFilter={this.handlePowerFilter}
+              handleWeightFilter={this.handleWeightFilter}
+              handleTypeFilter={this.handleTypeFilter}
+            />
+          </FilterContainer>
+          <Cards 
+            scooters={filteredScooters}
+          />
         </ContentWrapper>
       </AppWrapper>
     );
